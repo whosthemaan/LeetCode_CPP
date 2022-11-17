06569411651859f81b6c9b78209e0cabd55771e7
Rohan Maan <rohanmaan@gmail.com>
2022-11-17 02:43:04 -0500

    541 remaining

--- 541. Reverse String 2/reverse_string_2.cpp
+++ 541. Reverse String 2/reverse_string_2.cpp
@@ -5,14 +5,45 @@ using namespace std;
 
 string reverseStr(string s, int k) {
     int sz = s.size();
-    int remaining = sz%(2*k);
+    int count = ((sz-1)/k);
     int start = 0;
     int end = k-1;
+    int remaining = sz - (count*k);
+    int rem_new = sz - ((count)*k);
 
-    for(int i=start;i<end;i++)
+    for(int num=1;num<=remaining;num++)
     {
-        swap(s[start], s[end]);
-        end--;
+        for(int i=start;i<end;i++)
+        {
+            swap(s[start], s[end]);
+            end--;
+        }
+        start = num*(2*k);
+        end = start + k-1;
+    }
+
+    // cout << remaining << " " << rem_new;
+    if(remaining < k && remaining!=rem_new)
+    {
+        start = (count-1)*k;
+        end = sz-1;
+
+        for(int i=start;i<end;i++)
+        {
+            swap(s[start], s[end]);
+            end--;
+        }
+    }
+    else if(rem_new<(2*k) && rem_new>=k)
+    {
+        start = (count-1)*k;
+        end = start + k + 1;
+
+        for(int i=start;i<end;i++)
+        {
+            swap(s[start], s[end]);
+            end--;
+        }
     }
 
     return s;
@@ -20,7 +51,7 @@ string reverseStr(string s, int k) {
 
 int main()
 {
-    string str = "rohan";
+    string str = "abcd";
     int k = 2;
     string new_str = reverseStr(str, k);
     cout << new_str;
