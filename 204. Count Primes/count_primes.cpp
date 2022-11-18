76a83eeb2a65b3fce9c2fcf0abad2ca932d5fa1c
Rohan Maan <rohanmaan@gmail.com>
2022-11-18 15:17:48 -0500

    updating 204

--- 204. Count Primes/count_primes.cpp
+++ 204. Count Primes/count_primes.cpp
@@ -2,33 +2,6 @@
 #include <vector>
 using namespace std;
 
-// bool isprime(int n)
-// {
-//     if(n<=1)
-//         return false;
-
-//     for(int i=2;i<n;i++)
-//     {
-//         if(n%i==0)
-//         {
-//             return false;
-//         }
-//     } 
-//     return true;
-// }
-
-// int countPrimes(int n) {
-//     int count = 0;
-
-//     for(int i=2;i<n;i++)
-//     {
-//         if(isprime(i))
-//             count++;
-//     }
-
-//     return count;
-// }
-
 bool countPrimes(int n)
 {
     int count = 0;
