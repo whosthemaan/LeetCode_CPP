6d301dd35c0c9018bc74af05e9d285073427a7f7
whosthemaan <rohanmaan@gmail.com>
2023-03-25 10:41:08 -0400

    adding 383

---
+++ 383_Ransom_Note.py
@@ -0,0 +1,10 @@
+
+def canConstruct(ransomNote: str, magazine: str) -> bool:
+    for i in range(len(ransomNote)):
+        if(magazine.count(ransomNote[i])>0):
+            magazine = magazine.replace(str(ransomNote[i]), '', 1)
+        else:
+            return False
+    return True
+
+print(canConstruct("aa", "ab"))
